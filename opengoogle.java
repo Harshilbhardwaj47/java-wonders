4948e54d2e97e5de41b98a9b0cb7733156fd046f
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-04-21 20:14:40 -0700

    open google search using java and selinium

---
+++ opengoogle.java
@@ -0,0 +1,30 @@
+
+import org.openqa.selenium.Keys;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class first {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String search = in.nextLine();
+		// declaration and instantiation of objects/variables
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		// Launch website
+		driver.navigate().to("http://www.google.com/");
+
+		// Click on the search text box and send value
+
+		driver.findElement(By.name("q")).sendKeys(search);
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+	}
+
+}
+
