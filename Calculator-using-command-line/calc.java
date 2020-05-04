d940d1757d2227d94a22c2312a6713175f82fa8b
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-05-04 16:02:23 -0700

    command line calculator

---
+++ Calculator-using-command-line/calc.java
@@ -0,0 +1,20 @@
+package exp2;
+
+public class calc {
+	public static void main(String[] args) {
+		int num1 = int.parseInt(args[0]);
+		String operator = args[1];
+		int num2 = int.parseInt(args[2]);
+		int operation = 0;
+		if (operator.contentEquals("+")) // content equals is used to equate any character
+			operation = num1 + num2;
+		if (operator.contentEquals("*"))
+			operation = num1 * num2;
+		if (operator.contentEquals("-"))
+			operation = num1 - num2;
+		if (operator.contentEquals("/"))
+			operation = num1 / num2;
+		System.out.println("answer of " + operator + ": " + operation);
+	}
+}
+
