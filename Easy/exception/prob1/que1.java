aec150c38b59621dbbf36dde8b1b9c3a59e72aa2
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-05-04 21:25:02 -0700

    exception on array handeled

---
+++ Easy/exception/prob1/que1.java
@@ -0,0 +1,33 @@
+package exp7;
+
+import java.lang.ArrayIndexOutOfBoundsException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+class exp7 {
+	String[] name = new String[5];
+	int[] rollno = new int[5];
+}
+
+public class que1 {
+	public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
+		exp7 obj = new exp7();
+		for (int i = 0; i < obj.rollno.length; i++) {
+			try { // identification of error in try block
+				Scanner in = new Scanner(System.in);
+				System.out.println("Enter the name of the student");
+				obj.name[i] = in.nextLine();
+				System.out.println("Enter the roll no of the student");
+				obj.rollno[i] = in.nextInt();
+			}
+
+			catch (ArrayIndexOutOfBoundsException e) // catching the errors
+			{
+				System.out.println("index is exceeding");
+			}
+		}
+		System.out.println(Arrays.toString(obj.name));
+		System.out.println(Arrays.toString(obj.rollno));
+	}
+}
+
