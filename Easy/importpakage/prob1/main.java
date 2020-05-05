68273df2abf318212807ee91c28f49d267f3e9dd
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-05-04 20:47:47 -0700

    class that imports pac class

---
+++ Easy/importpakage/prob1/main.java
@@ -0,0 +1,14 @@
+package exp601;
+
+import exp6.pac;
+
+public class main {
+
+	public static void main(String[] args) {
+		pac obj = new pac();
+		System.out.println("value of num1 is: " + obj.num1);
+		System.out.println("value of num2 is: " + obj.num2);
+	}
+
+}
+
