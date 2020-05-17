6154a15c157b44d49cde24e992e16dd84be67389
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-05-16 20:17:55 -0700

    Button swing

---
+++ Hard/JFrame/Button/Button.java
@@ -0,0 +1,19 @@
+package walkthrough;
+
+import javax.swing.*;
+
+public class Button {
+	public static void main(String[] args) {
+		JFrame f = new JFrame();// creating instance of JFrame
+
+		JButton b = new JButton("Button");// creating instance of JButton
+		b.setBounds(200, 200, 100, 100);// x axis, y axis, width, height
+
+		f.add(b);// adding button in JFrame
+
+		f.setSize(500, 500);// 400 width and 500 height
+		f.setLayout(null);// using no layout managers
+		f.setVisible(true);// making the frame visible
+	}
+}
+
