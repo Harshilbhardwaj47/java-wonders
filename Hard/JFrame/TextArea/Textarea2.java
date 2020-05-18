8926985172ee63ef18d6e0c2d107c96e03a5bd77
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-05-18 16:23:53 -0700

    made few changes

--- Hard/JFrame/TextArea/Textarea2.java
+++ Hard/JFrame/TextArea/Textarea2.java
@@ -10,35 +10,35 @@ public class Textarea2 implements ActionListener {
 	private ActionEvent e;
 
 	Textarea2() {
-		JFrame f = new JFrame("Character counter");
-		l1 = new JLabel();
-		l1.setBounds(50, 25, 100, 30);
-		l2 = new JLabel();
-		l2.setBounds(160, 25, 100, 30);
-		area = new JTextArea();
-		area.setBounds(20, 75, 250, 200);
-		b = new JButton("Count Words");
-		b.setBounds(100, 300, 120, 30);
-		b.addActionListener(this);
-		f.add(l1);
-		f.add(l2);
-		f.add(area);
-		f.add(b);
-		f.setSize(450, 450);
-		f.setLayout(null);
-		f.setVisible(true);
-		actionPerformed(e);
+		JFrame f = new JFrame("Character counter");// this instance for frame
+		l1 = new JLabel();// same
+		l1.setBounds(50, 25, 100, 30);// same
+		l2 = new JLabel();// same 
+		l2.setBounds(160, 25, 100, 30);// same 
+		area = new JTextArea();// instance for text  area
+		area.setBounds(20, 75, 250, 200);// setting the position and the size
+		b = new JButton("Count Words");// createing button instance
+		b.setBounds(100, 300, 120, 30);// definig the axis and dimension for the wordas
+		b.addActionListener(this);// using the extended class action listner
+		f.add(l1);// adding the label in the frame
+		f.add(l2);// adding the label in the frame
+		f.add(area);// adding the textarea int the frame
+		f.add(b);// addint the button
+		f.setSize(450, 450);// setting the size of frame size
+		f.setLayout(null);// seting layout instcance to null
+		f.setVisible(true);// setting the instance to visible
+		actionPerformed(e);// calling the method action performed
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		String text = area.getText();
-		String words[] = text.split("\\s");
-		l1.setText("Words: " + words.length);
-		l2.setText("Characters: " + text.length());
+		String text = area.getText();// get the text in the given text area
+		String words[] = text.split("\\s");// spliting texts thropugh space tou count number of wordz
+		l1.setText("Words: " + words.length);// printing out the number of words
+		l2.setText("Characters: " + text.length());// printing out the number of characters
 	}
 
 	public static void main(String[] args) {
-		Textarea2 ob = new Textarea2();
+		Textarea2 ob = new Textarea2();// class counstructor calling through objects
 	}
 }
 
